8f12f3f1c6c250406a5ae688e7b0db8dcba4e02e
alferon <alferon@IUTCLINFC2118>
2022-10-29 09:35:56 +0200

    Htlm page

--- BlazorApp/BlazorApp/Pages/List.razor.cs
+++ BlazorApp/BlazorApp/Pages/List.razor.cs
@@ -1,6 +1,21 @@
-﻿namespace BlazorApp.Pages
+﻿using BlazorApp.Model;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Pages
 {
     public partial class List
     {
+        private Item[]? items;
+
+        [Inject]
+        public HttpClient? Http { get; set; }
+
+        [Inject]
+        public NavigationManager? NavigationManager { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            items = await Http.GetFromJsonAsync<Item[]>($"{NavigationManager.BaseUri}fake-data.json");
+        }
     }
 }
