895061a8993ad782f9b59cd6ca3bf9c6e985ff31
Alexis FERON <alferon>
2022-11-23 14:48:02 +0100

    Start of the project

--- BlazorApp/BlazorApp/Pages/Index.razor.cs
+++ BlazorApp/BlazorApp/Pages/Index.razor.cs
@@ -7,26 +7,5 @@ namespace BlazorApp.Pages
 {
     public partial class Index
     {
-        [Inject]
-        public IDataService DataService { get; set; }
-
-        public List<Item> Items { get; set; } = new List<Item>();
-
-        private List<CraftingRecipe> Recipes { get; set; } = new List<CraftingRecipe>();
-
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            base.OnAfterRenderAsync(firstRender);
-
-            if (!firstRender)
-            {
-                return;
-            }
-
-            Items = await DataService.List(0, await DataService.Count());
-            Recipes = await DataService.GetRecipes();
-
-            StateHasChanged();
-        }
     }
 }
