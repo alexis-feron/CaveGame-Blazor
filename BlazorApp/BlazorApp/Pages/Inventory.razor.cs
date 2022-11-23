ce000dc13959dce7e7b05126b00b930a238dfdf0
alferon <alferon@IUTCLINFB1816>
2022-11-23 15:17:54 +0100

    Start of the page Inventory

---
+++ BlazorApp/BlazorApp/Pages/Inventory.razor.cs
@@ -0,0 +1,48 @@
+﻿using BlazorApp.Modals;
+using BlazorApp.Model;
+using BlazorApp.Services;
+using Blazored.Modal;
+using Blazored.Modal.Services;
+using Blazorise.DataGrid;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace BlazorApp.Pages
+{
+    public partial class Inventory
+    {
+
+        [Inject]
+        public IStringLocalizer<List> Localizer { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [CascadingParameter]
+        public IModalService Modal { get; set; }
+
+        private List<Item> items;
+
+        private int totalItem;
+
+        [Inject]
+        public IDataService DataService { get; set; }
+
+        [Inject]
+        public IWebHostEnvironment WebHostEnvironment { get; set; }
+
+        private async Task OnReadData(DataGridReadDataEventArgs<Item> e)
+        {
+            if (e.CancellationToken.IsCancellationRequested)
+            {
+                return;
+            }
+
+            if (!e.CancellationToken.IsCancellationRequested)
+            {
+                items = await DataService.List(e.Page, e.PageSize);
+                totalItem = await DataService.Count();
+            }
+        }
+    }
+}
